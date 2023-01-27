4c3903357d47b323b6130295e2950deb306663c2
Felipe Barreiro <114102258+Lipebarreiro@users.noreply.github.com>
2023-01-27 16:13:27 -0300

    Add files via upload
    Adição da validação da página matrícula

---
+++ src/components/validacao/matricula.js
@@ -0,0 +1,91 @@
+function matricula() {
+  const nome = document.getElementById("nomeCompleto").value;
+  const email = document.getElementById("emailform").value;
+  const nascimento = document.getElementById("dataform").value;
+  const cpf = document.getElementById("cpf").value;
+  const tel = document.getElementById("telefone").value;
+  const rua = document.getElementById("rua").value;
+  const numero = document.getElementById("numero").value;
+  const bairro = document.getElementById("bairro").value;
+  const cidade = document.getElementById("cidade").value;
+  const estado = document.getElementById("estado").value
+  const cep = document.getElementById("CEP").value;
+
+  const caixaErro = document.getElementById("textoerro");
+
+  const validacaoNome = nome.length < 5
+  const validacaoEmail = email.indexOf("@", 3) == -1 || email.indexOf(".com", email.indexOf("@")) == -1
+  const validacaoNascimento = nascimento.length < 10 || nascimento.length > 10
+  const validacaoCpf = cpf.length < 11 || cpf.length > 11
+  const validacaoTel = tel.length < 10 || tel.length > 11
+  const validacaoRua = rua.length < 3
+  const validacaoNumero = numero.length < 1
+  const validacaoBairro = bairro.length < 3
+  const validacaoCidade = cidade.length < 3
+  const validacaoEstado = estado.length < 3
+  const validacaoCep = cep.length !== 8
+
+  caixaErro.textContent = ''
+
+  if (validacaoNome || validacaoEmail || validacaoNascimento || validacaoCpf || validacaoTel || validacaoRua || validacaoNumero || validacaoBairro || validacaoCidade || validacaoEstado || validacaoCep) {
+    try {
+      if (validacaoNome)
+        throw caixaErro.innerHTML += `<p class="abc"> coloque um nome valido </p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoEmail)
+        throw caixaErro.innerHTML += `<p class="abc"> insira um email valido </p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoNascimento)
+        throw caixaErro.innerHTML += `<p class="abc">insira uma data valida</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoCpf) throw caixaErro.innerHTML += `<p class="abc">insira um cpf valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoTel)
+        throw caixaErro.innerHTML += `<p class="abc">insira um telefone valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoRua)
+        throw caixaErro.innerHTML += `<p class="abc">insira um nome de rua valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoNumero)
+        throw caixaErro.innerHTML += `<p class="abc">insira um numero valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoBairro)
+        throw caixaErro.innerHTML += `<p class="abc">insira um nome de bairro valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoCidade)
+        throw caixaErro.innerHTML += `<p class="abc">insira um nome de cidade valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoEstado)
+        throw caixaErro.innerHTML += `<p class="abc">insira um nome de estado valido</p>`;
+    } catch (erro) { }
+
+    try {
+      if (validacaoCep) throw caixaErro.innerHTML += `<p class="abc">insira um CEP valido</p>`;
+    } catch (erro) { }
+
+  } 
+  else {
+    alert('Matriculado com Sucesso')
+  }
+
+}
+
+export default matricula;
